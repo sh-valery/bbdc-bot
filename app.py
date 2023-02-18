7b7e40a9f485396e548205bd25b0fcd6fbc99781
Val <regval@pm.me>
2023-02-18 22:52:09 +0800

    refactor methods

--- app.py
+++ app.py
@@ -19,169 +19,161 @@ known_days = {}
 known_sessions = {}
 
 
-def get_sessions(cookies, storage):
-    auth_jwt = cookies[0]['value']
-    logging.info(f"session id: {auth_jwt}")
-    if auth_jwt is None:
-        raise Exception("no session id")
+class BBDCProcessor:
+    def __init__(self, config):
+        logging.info("parse config")
 
-    storage = storage['vuex']
-    session_jwt = json.loads(storage)['user']['authToken']
+        self._username = config["bbdc"]["username"]
+        self._password = config["bbdc"]["password"]
 
-    url = "https://booking.bbdc.sg/bbdc-back-service/api/booking/c2practical/listPracSlotReleased"
+        # bot
+        self._bot_token = config["telegram"]["token"]
+        self._chat_id = config["telegram"]["chat_id"]
 
-    payload = "{\n    \"courseType\": \"2B\",\n    \"insInstructorId\": \"\",\n    \"stageSubDesc\": \"Subject 1.1\",\n    \"subVehicleType\": \"Circuit\",\n    \"stageSubNo\": \"1.01\"\n}"
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.5 Safari/605.1.15\'',
-        'JSESSIONID': session_jwt,
-        'Authorization': auth_jwt,
-    }
+        # connect to chrome
+        logging.info("connect to selenium")
+        chrome_host = config["chromedriver"]["host"]
+        self.browser = webdriver.Remote(
+            '{:}/wd/hub'.format(chrome_host), DesiredCapabilities.CHROME)
 
-    response = requests.request("POST", url, headers=headers, data=payload)
-    return
+    def run(self):
+        logging.info("parse and notify")
+        try:
+            self.browser.get('https://booking.bbdc.sg/#/login?redirect=%2Fbooking%2Findex')
+            self._login()
+            self._open_practical_tab()
 
+            header = self._get_lesson_name()
+            logging.info(f"choose lesson: {header}")
 
-def app(config):
-    logging.info("parse config")
+            days = self._get_new_available_days()
+            logging.info(f"New days found: {days}")
+            if len(days) > 0:
+                send_message(self._bot_token, self._chat_id, f"{header} \n New days found: {days}")
 
-    username = config["bbdc"]["username"]
-    password = config["bbdc"]["password"]
+            # Wait for new available slots to be loaded
+            slots = self._get_new_available_slots()
+            logging.info(f"New slots found: {slots}")
+            if len(slots) > 0:
+                send_message(self._bot_token, self._chat_id, f"{header} \n New slots found:\n{''.join(slots)}")
 
-    # bot
-    bot_token = config["telegram"]["token"]
-    chat_id = config["telegram"]["chat_id"]
-    enable_bot = config["telegram"]["enabled"]
+            # parse calendar
+            while True:
+                logging.info("parsing calendar...")
+                self._parse_and_notify()
+                logging.info("wait for 30-150 seconds before refresh...")
+                sleep(random.randint(30, 150))
+                logging.info("refreshing...")
+                self.browser.refresh()
 
-    # chrome host
-    chrome_host = config["chromedriver"]["host"]
+        except Exception as e:
+            logging.exception(e)
+            send_message(self._bot_token, self._chat_id, f"[Error]\n{e}")
+            raise
+        finally:
+            self.browser.quit()
 
-    # implicit_wait = 5
-    # browser.implicitly_wait(implicit_wait)
+    def _login(self):
+        logging.info("login")
 
-    # connect to chrome
-    logging.info("connect to selenium")
-    browser = webdriver.Remote(
-        '{:}/wd/hub'.format(chrome_host), DesiredCapabilities.CHROME)
-    browser.get('https://booking.bbdc.sg/#/login?redirect=%2Fbooking%2Findex')
+        # Wait for the login form to be loaded
+        wait = WebDriverWait(self.browser, 10)
+        idLogin = wait.until(EC.presence_of_element_located((By.ID, 'input-8')))
+        idLogin.send_keys(self._username)
+        idPassword = wait.until(EC.presence_of_element_located((By.ID, 'input-15')))
+        idPassword.send_keys(self._password)
 
-    try:
-        # login BBDC
-        login(browser, password, username)
+        # Wait for the login button to be clickable
+        loginButton = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'v-btn')))
+        loginButton.click()
+        self.browser.switch_to.default_content()
 
-        # proceed unsure form (Chrome)
-        browser.switch_to.default_content()
-
-        # click practical button
-        sleep(20)
+    def _open_practical_tab(self):
+        wait = WebDriverWait(self.browser, 10)
+        practical = wait.until(
+            EC.element_to_be_clickable((
+                By.XPATH,
+                '//*[@id="app"]/div/div/main/div/div/div[2]/div/div[1]/div/div/div[1]/div/div[2]/div/div[2]')))
         logging.info("click practical button")
-        practical = browser.find_element_by_xpath(
-            '//*[@id="app"]/div/div/main/div/div/div[2]/div/div[1]/div/div/div[1]/div/div[2]/div/div[2]')
         practical.click()
+
+    def _parse_and_notify(self):
+        wait = WebDriverWait(self.browser, 10)
+        wait.until(EC.presence_of_element_located((By.XPATH,
+                                                   '//button[@class="v-btn v-btn--is-elevated v-btn--has-bg theme--light v-size--default primary"]')))
+        logging.info("click booking button")
+        book_next = self.browser.find_elements_by_xpath(
+            '//button[@class="v-btn v-btn--is-elevated v-btn--has-bg theme--light v-size--default primary"]')[-1]
+
+        book_next.click()
 
         # if have booked lesson, click continue
         try:
-            continue_button = browser.find_element_by_xpath('/html/body/div[1]/div[3]/div/div/div[2]/button[2]')
+            continue_button = self.browser.find_element_by_xpath(
+                '/html/body/div[1]/div[3]/div/div/div[2]/button[2]')
             continue_button.click()
         except NoSuchElementException:
             logging.info("No continue button")
 
-        # GET COOKIE AND USE API
-        # sessions = get_sessions(browser.get_cookies(), browser.execute_script("return window.localStorage;"))
 
-        # parse calendar
-        while True:
-            logging.info("parsing calendar...")
-            parse_and_notify(bot_token, browser, chat_id)
-            logging.info("wait for 30-150 seconds before refresh...")
-            sleep(random.randint(30, 150))
-            logging.info("refreshing...")
-            browser.refresh()
+    def _get_new_available_slots(self):
+        wait = WebDriverWait(self.browser, 10)
 
-    except Exception as e:
-        logging.exception(e)
-        send_message(bot_token, chat_id, f"[Error]\n{e}")
-        raise
-    finally:
-        browser.quit()
+        wait.until(
+            EC.presence_of_element_located((By.XPATH, '//div[contains(@class, "available-slot__item__time")]')))
 
+        browser = self.browser
+        sessions = browser.find_elements_by_class_name('sessionList')
+        new_known_sessions = {}
+        sessions_to_notify = []
+        for s in sessions:
+            if s.text == '':  # skip empty blocks
+                continue
 
-def login(browser, password, username):
-    logging.info("login")
+            date, total, name, time, cost = s.text.splitlines()
+            logging.info(f"Session found: {date} {time}")
+            if f'{date} {time}' not in known_sessions:
+                known_sessions[f'{date} {time}'] = True
+                logging.warning(f"[NEW] New session found: {date} {time}")
+                sessions_to_notify.append(f"{date} {time}\n")
+            new_known_sessions[f'{date} {time}'] = True
 
-    # Wait for the login form to be loaded
-    wait = WebDriverWait(browser, 10)
-    idLogin = wait.until(EC.presence_of_element_located((By.ID, 'input-8')))
-    idLogin.send_keys(username)
-    idPassword = wait.until(EC.presence_of_element_located((By.ID, 'input-15')))
-    idPassword.send_keys(password)
+        # refresh known sessions
+        known_sessions.clear()
+        known_sessions.update(new_known_sessions)
+        return sessions_to_notify
 
-    # Wait for the login button to be clickable
-    loginButton = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'v-btn')))
-    loginButton.click()
+    def _get_new_available_days(self):
+        wait = WebDriverWait(self.browser, 10)
 
+        wait.until(
+            EC.presence_of_element_located((By.XPATH, '//div[contains(@class, "available-day__day__date")]')))
 
-def parse_and_notify(bot_token, browser, chat_id):
-    # Wait for the booking button to be clickable
-    wait = WebDriverWait(browser, 10)
-    book_next = wait.until(EC.element_to_be_clickable((By.XPATH, '//button[@class="v-btn v-btn--is-elevated v-btn--has-bg theme--light v-size--default primary"]')))
-    logging.info("click booking button")
-    book_next.click()
-    header = book_next.text # todo: header should be the lesson name, not the button text
-    logging.info(f"choose lesson: {header}")
+        browser = self.browser
+        calendar = browser.find_element_by_xpath(
+            '/html/body/div[1]/div/div/main/div/div/div[2]/div/div[2]/div[1]/div[1]/div[1]/div[4]/div/div/div')
 
-    # Wait for new available days to be loaded
-    wait.until(EC.presence_of_element_located((By.XPATH, '//div[contains(@class, "available-day__day__date")]')))
-    days = get_new_available_days(browser)
-    logging.info(f"New days found: {days}")
-    if len(days) > 0:
-        send_message(bot_token, chat_id, f"{header} \n New days found: {days}")
+        days_to_notify = []
+        new_known_days = {}
+        for day in calendar.find_elements_by_class_name('v-btn__content'):
+            logging.info(f"Day found: {day.text}")
+            new_known_days[day.text] = True
+            if day.text not in known_days:
+                days_to_notify.append(day.text)
+                logging.warning(f"[NEW] New day found: {day.text}")
+            day.click()
+            sleep(random.randint(2, 4))
 
-    # Wait for new available slots to be loaded
-    wait.until(EC.presence_of_element_located((By.XPATH, '//div[contains(@class, "available-slot__item__time")]')))
-    slots = get_new_available_slots(browser)
-    logging.info(f"New slots found: {slots}")
-    if len(slots) > 0:
-        send_message(bot_token, chat_id, f"{header} \n New slots found:\n{''.join(slots)}")
+        # refresh known days
+        known_days.clear()
+        known_days.update(new_known_days)
+        return days_to_notify
 
+    def _get_lesson_name(self) -> str:
+        try:
+            self.browser.find_element_by_xpath('//p[@class="title d-block d-md-none"]')
+            return self.browser.find_element_by_xpath('//p[@class="title d-block d-md-none"]').text
+        except NoSuchElementException:
+            logging.info("No header for the lesson")
+            return "Unknow lesson name"
 
-def get_new_available_slots(browser):
-    sessions = browser.find_elements_by_class_name('sessionList')
-    new_known_sessions = {}
-    sessions_to_notify = []
-    for s in sessions:
-        if s.text == '':  # skip empty blocks
-            continue
-
-        date, total, name, time, cost = s.text.splitlines()
-        logging.info(f"Session found: {date} {time}")
-        if f'{date} {time}' not in known_sessions:
-            known_sessions[f'{date} {time}'] = True
-            logging.warning(f"[NEW] New session found: {date} {time}")
-            sessions_to_notify.append(f"{date} {time}\n")
-        new_known_sessions[f'{date} {time}'] = True
-
-    # refresh known sessions
-    known_sessions.clear()
-    known_sessions.update(new_known_sessions)
-    return sessions_to_notify
-
-
-def get_new_available_days(browser):
-    calendar = browser.find_element_by_xpath(
-        '/html/body/div[1]/div/div/main/div/div/div[2]/div/div[2]/div[1]/div[1]/div[1]/div[4]/div/div/div')
-
-    days_to_notify = []
-    new_known_days = {}
-    for day in calendar.find_elements_by_class_name('v-btn__content'):
-        logging.info(f"Day found: {day.text}")
-        new_known_days[day.text] = True
-        if day.text not in known_days:
-            days_to_notify.append(day.text)
-            logging.warning(f"[NEW] New day found: {day.text}")
-        day.click()
-        sleep(random.randint(2, 4))
-
-    # refresh known days
-    known_days.clear()
-    known_days.update(new_known_days)
-    return days_to_notify
