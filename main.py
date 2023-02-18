37e8d703f028466ce52c4f5d19cc90d9b293e9a9
Val <regval@pm.me>
2023-02-18 23:00:40 +0800

    simplify main

---
+++ main.py
@@ -0,0 +1,15 @@
+from app import BBDCProcessor
+import schedule
+import time
+from config import load_config
+
+# load config
+config = load_config("config_prod.yaml")
+
+if __name__ == "__main__":
+    try:
+        bbdc = BBDCProcessor(config)
+        bbdc.run()
+    except Exception as e:
+        print(e)
+
